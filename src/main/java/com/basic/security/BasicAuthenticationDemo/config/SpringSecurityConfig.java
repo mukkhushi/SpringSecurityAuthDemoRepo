5c54e6461b79efc745efe16e9d0fd90069b08078
Mukesh Kumar <mukeshkumarray012@gmail.com>
2026-04-03 21:46:48 +0530

    Role and Authorities based Authorization implementation has completed

--- src/main/java/com/basic/security/BasicAuthenticationDemo/config/SpringSecurityConfig.java
+++ src/main/java/com/basic/security/BasicAuthenticationDemo/config/SpringSecurityConfig.java
@@ -1,15 +1,14 @@
 package com.basic.security.BasicAuthenticationDemo.config;
 
 import com.basic.security.BasicAuthenticationDemo.filters.JwtAuthFilter;
-import com.basic.security.BasicAuthenticationDemo.model.Permissions;
 import com.basic.security.BasicAuthenticationDemo.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -21,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SpringSecurityConfig {
 
     @Autowired
@@ -30,9 +30,8 @@ public class SpringSecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                         .authorizeHttpRequests(auth-> auth
-                                .requestMatchers(HttpMethod.GET,"/create").hasAllAuthorities(Permissions.EMPLOYEE_READ.name(),Permissions.EMPLOYEE_WRITE.name(),Permissions.EMPLOYEE_DELETE.name())
-                                .requestMatchers(HttpMethod.POST,"/findALl").hasAllAuthorities(Permissions.EMPLOYEE_READ.name())
-
+                                //.requestMatchers(HttpMethod.GET,"/create").hasAllAuthorities(Permissions.EMPLOYEE_READ.name(),Permissions.EMPLOYEE_WRITE.name(),Permissions.EMPLOYEE_DELETE.name())
+                                //.requestMatchers(HttpMethod.POST,"/findALl").hasAllAuthorities(Permissions.EMPLOYEE_READ.name())
                                 .requestMatchers("/authenticate").permitAll()
                                 .anyRequest().authenticated()
                         );
--- src/main/java/com/basic/security/BasicAuthenticationDemo/controller/EmployeeController.java
+++ src/main/java/com/basic/security/BasicAuthenticationDemo/controller/EmployeeController.java
@@ -1,8 +1,10 @@
 package com.basic.security.BasicAuthenticationDemo.controller;
 
 import com.basic.security.BasicAuthenticationDemo.model.Employee;
+import com.basic.security.BasicAuthenticationDemo.model.Permissions;
 import com.basic.security.BasicAuthenticationDemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -15,6 +17,7 @@ public class EmployeeController {
 
     //save employee
     @PostMapping("/create")
+    @PreAuthorize("hasAuthority('EMPLOYEE_WRITE')")
     String createEmployee(@RequestBody Employee employee){
         Employee employee1 = employeeService.createEmployee(employee);
         if(employee1 != null){
@@ -40,6 +43,7 @@ public class EmployeeController {
     }
 
     @GetMapping("/findAll")
+    @PreAuthorize("hasRole('USER')")
     List<Employee> getAllEmployee(){
         return employeeService.getAllEmployees();
     }
